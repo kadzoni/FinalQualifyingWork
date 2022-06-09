2434f71e512198cba9eeda2c76a0843eaa7389d0
Kirill Kniga <kadzoni@gmail.com>
2022-06-09 22:50:17 +0500

    еее удалось обойти трабл с промисами

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,10 +1,11 @@
 const HomeService = require('../services/home.service')
-
+const { PrismaClient } = require('@prisma/client'),
+      prisma = new PrismaClient()
 class HomeController {
-    getPage(req, res) {
+    async getPage(req, res) {
         return res
             .status(200)
-            .render('home', { title: 'Greetings form Handlebars', AllRooms: JSON.stringify(HomeService.getChatRoom())})
+            .render('home', { title: 'Greetings form Handlebars', AllRooms: JSON.stringify(await prisma.chat_room.findMany())})
     }
 }
 module.exports = new HomeController()
--- services/home.service.js
+++ services/home.service.js
@@ -1,23 +1,8 @@
-const mysql = require("mysql2");
-  
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USERNAME || "root",
-  database: process.env.DB_DATABASE="chatdatabase",
-}).promise();;
+const { PrismaClient } = require('@prisma/client'),
+      prisma = new PrismaClient()
 
 class HomeService{
-    getChatRoom(){
-        const sql = `SELECT * FROM chat_room`;
-        return connection.query(sql).then(result =>{
-            console.log(result);
-            connection.end();
-          })
-          .catch(err =>{
-            console.log(err);
-            connection.end();
-          });
-    }
+
 }
 
 module.exports = new HomeService()
